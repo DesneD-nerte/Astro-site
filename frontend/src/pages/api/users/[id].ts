a9770f298af2b15529fd47388ef0773e2e2fa691
Alexandr Ershov <ershov.avdd@gmail.com>
2022-10-04 02:37:28 +0700

    Cart component user put

---
+++ frontend/src/pages/api/users/[id].ts
@@ -0,0 +1,34 @@
+import type { Params } from "astro";
+import { getFetchResponse } from "../../../utils/fetch";
+
+export async function post({ params, request }: { params: Params; request: Request }) {
+  const id = params;
+  const body = await request.json();
+
+  return await getFetchResponse(request, `http://localhost:1337/api/users/${id}`, {
+    method: "",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      Cart: {
+        products: {
+          id: body,
+        },
+      },
+    }),
+  });
+}
+
+export async function put({ params, request }: { params: Params; request: Request }) {
+  const id = params;
+  const body = await request.json();
+
+  return await getFetchResponse(request, `http://localhost:1337/api/users/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  });
+}
