f198edb2ecca945a2fcee2b50d397cdc24c32b05
Alexandr Ershov <ershov.avdd@gmail.com>
2022-10-07 02:35:56 +0700

    Some fixes with basket page

--- strapi-api/dist/src/api/product/controllers/product.js
+++ strapi-api/dist/src/api/product/controllers/product.js
@@ -5,19 +5,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const strapi_1 = require("@strapi/strapi");
 exports.default = strapi_1.factories.createCoreController("api::product.product", ({ strapi }) => ({
-    async create(ctx) {
-        const { data, meta } = await super.create(ctx);
-        strapi.$io.raw("productsChanged", data);
-        return { data, meta };
-    },
     async update(ctx) {
-        const { data, meta } = await super.update(ctx);
-        strapi.$io.raw("productsChanged", data);
-        return { data, meta };
-    },
-    async delete(ctx) {
-        const { data, meta } = await super.delete(ctx);
-        strapi.$io.raw("productsChanged", data);
-        return { data, meta };
+        const response = await super.update(ctx);
+        strapi.$io.raw("productsChanged", response);
+        return response;
     },
 }));
--- strapi-api/src/api/product/controllers/product.ts
+++ strapi-api/src/api/product/controllers/product.ts
@@ -7,28 +7,12 @@ import { factories } from "@strapi/strapi";
 export default factories.createCoreController(
   "api::product.product",
   ({ strapi }: any) => ({
-    async create(ctx) {
-      const { data, meta } = await super.create(ctx);
-
-      strapi.$io.raw("productsChanged", data);
-
-      return { data, meta };
-    },
-
     async update(ctx) {
-      const { data, meta } = await super.update(ctx);
+      const response = await super.update(ctx);
 
-      strapi.$io.raw("productsChanged", data);
+      strapi.$io.raw("productsChanged", response);
 
-      return { data, meta };
-    },
-
-    async delete(ctx) {
-      const { data, meta } = await super.delete(ctx);
-
-      strapi.$io.raw("productsChanged", data);
-
-      return { data, meta };
+      return response;
     },
   })
 );
